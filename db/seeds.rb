e0f9f9036a6ab72fbcd3d3744589c3e783c032fc
ebhinch <erica.hinchman@gmail.com>
2017-11-07 20:39:32 -0500

    creating github repo. just built out rails routes and created react app. foreman working

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Creature.destroy_all
+
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
+Creature.create({name: "Skylar", description: "Tall, dark and handsome"})
+Creature.create({name: "Victoria", description: "Prettier than Princess Leia"})
